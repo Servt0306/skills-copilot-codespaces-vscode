4811d92c5021ac7344856179f38c12cbd48f47df
Servt0306 <48553947+Servt0306@users.noreply.github.com>
2024-02-19 07:25:01 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server
+// http.createServer(function(req, res) {
+//     var url = req.url;
+//     console.log(url);
+//     if (url === '/comments') {
+//         res.writeHead(200, {
+//             'Content-Type': 'text/plain'
+//         });
+//         res.end('Comments');
+//     } else {
+//         res.writeHead(404, {
+//             'Content-Type': 'text/plain'
+//         });
+//         res.end('Not Found');
+//     }
+// }).listen(3000, '
